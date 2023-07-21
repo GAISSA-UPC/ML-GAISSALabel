c74cd56c271e66f72fc2fecb5a39567524b9cf0d
Pau Duran <pauduranmanzano@gmail.com>
2023-07-21 15:32:05 +0200

    Inicialització backend + primers models i vistes

---
+++ backend/api/urls.py
@@ -0,0 +1,13 @@
+from rest_framework import routers
+from django.urls import path, include
+from . import views
+
+router = routers.DefaultRouter()
+router.register(r'models', views.ModelsView, basename='models')
+router.register(r'models/(?P<model_id>\d+)/entrenaments', views.EntrenamentsView, basename='entrenaments')
+router.register(r'metriques', views.MetriquesView, basename='metriques')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+]
