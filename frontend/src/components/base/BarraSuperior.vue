b6a7f1d4d8bdebf31cc6dbf69aa42e8a84b81d4a
Pau Duran <pauduranmanzano@gmail.com>
2023-07-25 16:46:01 +0200

    Continuant amb l'estructura base del front

--- frontend/src/components/base/BarraSuperior.vue
+++ frontend/src/components/base/BarraSuperior.vue
@@ -1,24 +1,39 @@
 <template>
-    <span style="width: 100%; display: flex; align-items: center">
-        <el-button>
-            <font-awesome-icon :icon="['fas', 'bars']" />
-        </el-button>
-        <el-image
-            src="https://gaissa.upc.edu/en/shared/GAISSALOGO.png/@@images/9e425bf5-ac52-418a-84fa-719ae8fedbf8.png"
-            style="cursor:pointer; max-width: 40px"
-            @click="obrirLink"
-        />
-        <span class="text-large font-600 mr-3"> Tool's name :) </span>
-        <el-button type="primary" class="ml-2">{{ $t('Admin') }}</el-button>
+    <span style="width: 100%; display: flex; justify-content: space-between">
+        <div style="display: flex; align-items: center">
+            <el-button @click="canviCollapse">
+                <font-awesome-icon :icon="['fas', 'bars']" />
+            </el-button>
+            <el-image
+                src="https://gaissa.upc.edu/en/shared/GAISSALOGO.png/@@images/9e425bf5-ac52-418a-84fa-719ae8fedbf8.png"
+                style="cursor:pointer; max-width: 40px; margin-left: 20px; margin-right: 5px"
+                @click="obrirLink"
+            />
+            <span class="text-large font-600 mr-3"> Tool's name :) </span>
+        </div>
+        <div>
+            <el-button type="primary" class="ml-2">{{ $t('Admin') }}</el-button>
+        </div>
     </span>
 </template>
 
 <script>
 export default {
     name: "BarraSuperior",
+    props: {
+        collapsed: {required: false, type: Boolean, default: false}
+    },
+    emits: ['collapse', 'expand'],
     methods: {
         obrirLink() {
             window.open('https://gaissa.upc.edu/en', '_blank');
+        },
+        canviCollapse() {
+            if (this.collapsed) {
+                this.$emit('expand')
+            } else {
+                this.$emit('collapse')
+            }
         }
     }
 }
