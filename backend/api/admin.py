b9db7effcff3b9539d95f10b48145e47e3beaef1
Pau Duran <pauduranmanzano@gmail.com>
2023-07-31 16:02:04 +0200

    Afegint models qualificacio i interval

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -4,4 +4,6 @@ from . import models
 admin.site.register(models.Model)
 admin.site.register(models.Entrenament)
 admin.site.register(models.Metrica)
+admin.site.register(models.Qualificacio)
+admin.site.register(models.Interval)
 admin.site.register(models.ResultatEntrenament)
