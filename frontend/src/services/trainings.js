2ee09094fced598dc645fddeb11dd08efa2cfb44
Pau Duran <pauduranmanzano@gmail.com>
2023-08-01 12:21:06 +0200

    Eliminant console.logs posats per debugar

--- frontend/src/services/trainings.js
+++ frontend/src/services/trainings.js
@@ -3,8 +3,6 @@ import axios from "@/services/backend"
 export default {
     async retrieve(model_id, training_id) {
         const responseTraining = await axios.get(`/api/models/${model_id}/entrenaments/${training_id}.json`)
-        console.log("responseTraining")
-        console.log(responseTraining)
         return responseTraining
     }
 }
