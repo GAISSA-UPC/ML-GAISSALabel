c597ef5038d9630c5dff0395d4bd619c7bc6c316
Pau Duran <pauduranmanzano@gmail.com>
2023-08-18 17:52:52 +0200

    Refactor de label generator en procés

--- backend/api/label_generator.py
+++ backend/api/label_generator.py
@@ -1,42 +1,22 @@
 import os
-import pandas as pd
 import numpy as np
 from io import BytesIO
+
+from reportlab.lib.utils import ImageReader
 from reportlab.pdfgen.canvas import Canvas
 from reportlab.lib.colors import black, white
 import qrcode
+from PIL import Image
 
 # Canvas size
 C_SIZE = (1560, 2411)
 
 # Positions for specific elements on the label, calculated as ratios of the overall canvas size
-co2_x, co2_y = 650/1560, 725/2655
-downloads_x, downloads_y = 1150/1560, 725/2655  
-top_x, top_y = 150/1560, 725/2655  
-parameters_x, parameters_y = 400/1560, 200/2655
-dataset_x, dataset_y = 950/1560, 200/2655
-
-# Mapping of label position keys to their respective attributes
-# Each entry is a tuple containing: the drawing method for the text, font size, font style, x position, y position, and formatting instructions
-POS_TEXT = {
-    # infos that are directly taken from summary via keys
-    "modelId":                                  ('drawString',        90, '-Bold', .04,  .855, None),
-    "task_type":                                ('drawString',        90, '',      .04,  .815, None),
-    "datasets":                                 ('drawRightString',   90, '',      .95,  .815, None),
-    "downloads":                                ('drawRightString',   68, '-Bold',  downloads_x+0.11,  downloads_y-0.025,  None),
-    "size_efficency":                           ('drawRightString',   68, '-Bold',  parameters_x+0.16,  parameters_y-0.025,  ''),
-    "datasets_size_efficency":                  ('drawRightString',   68, '-Bold',  dataset_x+0.15,  dataset_y-0.025,  ''),
-    "co2_eq_emissions":                         ('drawRightString',   68, '-Bold',  co2_x+0.16,  co2_y-0.025,  ''),
-    "performance_score":                        ('drawRightString',   68, '-Bold',  top_x+0.1, top_y-0.025, None),
-
-    # static infos, depending on $task
-    "$CO2e per Training":                       ('drawCentredString', 56, '',       co2_x+0.09,  co2_y-0.05,  None),
-    "$Downloads":                               ('drawCentredString', 56, '',       downloads_x+0.07,  downloads_y-0.05,  None),
-    "$Performance score":                       ('drawCentredString', 56, '',       top_x+0.06,  top_y-0.05,  None),
-    "$Model Size":                              ('drawCentredString', 56, '',       parameters_x+0.075,  parameters_y-0.05,  None),
-    "$Dataset Size":                            ('drawCentredString', 56, '',       dataset_x+0.075,  dataset_y-0.05,  None),
-
-}
+top_x, top_y = 260/1560, 650/2411
+co2_x, co2_y = 780/1560, 650/2411
+downloads_x, downloads_y = 1300/1560, 650/2411
+parameters_x, parameters_y = 520/1560, 200/2411
+dataset_x, dataset_y = 1040/1560, 200/2411
 
 
 # Mapping of icon names to their associated metrics
@@ -46,6 +26,14 @@ ICON_NAME_TO_METRIC = {
     'downloads': 'downloads',
     'top': 'performance_score',
     'dataset': 'datasets_size_efficency'
+}
+
+ICON_NAMES = {
+    'size_efficency': 'Size efficiency',
+    'datasets_size_efficency': 'Dataset size efficency',
+    'downloads': 'Descàrregues',
+    'performance_score': 'Performance score',
+    'co2_eq_emissions': 'CO2 emissions'
 }
 
 
@@ -61,6 +49,19 @@ ICON_POS = {
 
 # Directory of the label design elements
 PARTS_DIR = os.path.join(os.path.dirname(__file__), "label_design")
+
+
+def get_position(i, total):
+    if i == 0:
+        return 260 / 1560, 650 / 2411
+    elif i == 1:
+        return 780 / 1560, 650 / 2411
+    elif i == 2:
+        return 1300 / 1560, 650 / 2411
+    elif i == 3:
+        return 520 / 1560, 200 / 2411
+    else:
+        return 1040 / 1560, 200 / 2411
 
 
 def create_qr():
@@ -104,7 +105,7 @@ def draw_qr(canvas, qr, x, y, width):
         canvas.rect(x + (i * width), y + int(width * qr_pix.shape[0]) - ((j + 1) * width), width, width, fill=1, stroke=0)
 
 
-def generate_efficency_label(summary, meanings, frate, metric_to_rating):
+def generate_efficency_label(results, meanings, frate, model_name, task_type):
     """
     Args:
         summary (dict): Summary dictionary containing information about the model.
@@ -116,31 +117,10 @@ def generate_efficency_label(summary, meanings, frate, metric_to_rating):
     buffer = BytesIO()
     canvas = Canvas(buffer, pagesize=C_SIZE)
 
-    # Draw the background and the pictograms for the metrics
-    # Ratings are converted to images and drawn on the canvas
-    # If the rating is null, a "nan" image is drawn
+    # Draw the background
     canvas.drawInlineImage(os.path.join(PARTS_DIR, f"bg_new_logo.png"), 0, 0)
-    for icon, (posx, posy) in ICON_POS.items():
-        metric = ICON_NAME_TO_METRIC[icon]
-        rating = metric_to_rating[metric]
-        if pd.isnull(rating):
-            canvas.drawInlineImage(os.path.join(PARTS_DIR, f"nan.png"), posx+50, posy)
-        else:
-            canvas.drawImage(os.path.join(PARTS_DIR, f"{icon}_{rating}.png"), posx, posy)
 
-    # Position of the rating labels
-    POS_RATINGS = {char: (.66, y) for char, y in zip(meanings, reversed(np.linspace(.461, .727, 5)))}
-
-    # Draw the final rating and a QR code
-    if frate is None:
-        canvas.drawInlineImage(os.path.join(PARTS_DIR, f"nan.png"), POS_RATINGS['C'][0] * C_SIZE[0], POS_RATINGS['C'][1] * C_SIZE[1])
-    else:
-        canvas.drawInlineImage(os.path.join(PARTS_DIR, f"Rating_{frate}.png"), POS_RATINGS[frate][0] * C_SIZE[0], POS_RATINGS[frate][1] * C_SIZE[1])
-    qr = create_qr()
-    draw_qr(canvas, qr, 0.825 * C_SIZE[0], 0.894 * C_SIZE[1], 200)
-
-    # Text parts are created and added to the canvas
-    # If the metric is not available, it is represented as 'n.a.'
+    # Definition of text styles
     canvas.setFillColor(black)
     canvas.setLineWidth(3)
     canvas.setStrokeColor(black)
@@ -148,37 +128,63 @@ def generate_efficency_label(summary, meanings, frate, metric_to_rating):
     text.setTextRenderMode(2)
     canvas._code.append(text.getCode())
 
-    # Continue adding text to the canvas
-    for key, (draw_method, fsize, style, x, y, fmt) in POS_TEXT.items():
-        draw_method = getattr(canvas, draw_method)
-        canvas.setFont('Helvetica' + style, fsize)
+    # Draw model's name and task type
+    canvas.setFont('Helvetica-Bold', 90)
+    canvas.drawString(int(C_SIZE[0] * 0.04), int(C_SIZE[1] * 0.855), model_name)
+    canvas.setFont('Helvetica', 90)
+    canvas.drawString(int(C_SIZE[0] * 0.04), int(C_SIZE[1] * 0.815), task_type)
 
-        if key.startswith(f"$"): # Static text on label depending on the task type
-            text = key.replace(f"$", "")
-        elif key in summary: # Dynamic text that receives content from summary
-            if key in metric_to_rating.keys():
-                text = 'n.a.' if summary[key] is None else f'{summary[key]:4.2f}'
-                if text.endswith('.'):
-                    text = text[:-1]
-            else:
-                text = summary[key]
+    # Draw result, name and image of the metrics to the bottom of the canvas
+    i = 0
+    for metric, info in results.items():
+        # Unpack the information of the metric
+        value = str(round(info['value'], 2))
+        unit = info['unit']
+        image = info['image']
+
+        # Get the base position of the information to draw
+        posx, posy = get_position(i, len(results))
+
+        # Draw result (value + unit)
+        canvas.setFont('Helvetica-Bold', 68)
+        result = value + ' ' + unit if unit else value
+        canvas.drawCentredString(int(C_SIZE[0] * posx), int(C_SIZE[1] * (posy - 0.025)), result)
+
+        # Draw name of the metric
+        canvas.setFont('Helvetica', 54)
+        canvas.drawCentredString(int(C_SIZE[0] * posx), int(C_SIZE[1] * (posy - 0.05)), metric)
+
+        # Draw image of the metric
+        canvas.drawImage(ImageReader(Image.open(BytesIO(image))), int(C_SIZE[0] * posx - 125), int(C_SIZE[1] * posy))
+
+        i += 1
+
+    # Ratings are converted to images and drawn on the canvas
+    # If the rating is null, a "nan" image is drawn
+    """for icon, (posx, posy) in ICON_POS.items():
+        metric = ICON_NAME_TO_METRIC[icon]
+        rating = metric_to_rating[metric]
+        if pd.isnull(rating):
+            canvas.drawInlineImage(os.path.join(PARTS_DIR, f"nan.png"), posx+50, posy)
         else:
-            text = None
-        if text is not None: # Draw the text on the canvas
-            if fmt is not None:
-                units = summary['units']
-                if key in units:
-                    if units[key] in ['kg', 'kB']: # Convert units
-                        text = round(float(summary[key]) / 1000,2)
-                    if units[key] in ['t', 'MB']:
-                        text = round(float(summary[key]) / 1000000,2)
-                    if units[key] == 'GB':
-                        text = round(float(summary[key]) / 1000000000,2)
-                    text = f'{text} {units[key]}'
-                else:
-                    text = fmt.format(text)
+            canvas.drawImage(os.path.join(PARTS_DIR, f"{icon}_{rating}.png"), posx-125, posy)"""
 
-            draw_method(int(C_SIZE[0] * x), int(C_SIZE[1] * y), text)
+    # ToDo: Refactor --------------------------------------------------------------------------------------------------
+
+    # Position of the rating labels
+    POS_RATINGS = {char: (.66, y) for char, y in zip(meanings, reversed(np.linspace(.461, .727, 5)))}
+
+    # Draw the final rating and a QR code
+    if frate is None:
+        canvas.drawInlineImage(os.path.join(PARTS_DIR, f"nan.png"), POS_RATINGS['C'][0] * C_SIZE[0],
+                               POS_RATINGS['C'][1] * C_SIZE[1])
+    else:
+        canvas.drawInlineImage(os.path.join(PARTS_DIR, f"Rating_{frate}.png"), POS_RATINGS[frate][0] * C_SIZE[0],
+                               POS_RATINGS[frate][1] * C_SIZE[1])
+    qr = create_qr()
+    draw_qr(canvas, qr, 0.825 * C_SIZE[0], 0.894 * C_SIZE[1], 200)
+
+    # ToDo: -----------------------------------------------------------------------------------------------------------
 
     canvas.save()
     pdf = buffer.getvalue()
