1dda7724b37e3a2040fad81a1b3624f52abe31a6
Pau Duran <pauduranmanzano@gmail.com>
2023-09-18 11:04:01 +0200

    Refactor presaved per generalitzar training i inference

--- frontend/src/views/TrainingPreSaved.vue
+++ frontend/src/views/TrainingPreSaved.vue
@@ -1,94 +1,13 @@
 <template>
-    <h1>{{ $t("Energy label for training") }}</h1><br>
-    <h2>{{ $t("Create label for dataset model") }}</h2><br>
-    <div>
-        <el-form label-position="top">
-            <el-form-item :label="$t('Model')">
-                <el-select
-                    v-model="selectedModel"
-                    @change="canviModel"
-                >
-                    <el-option
-                        v-for="(model, i) in models" :key="i"
-                        :value="model.id"
-                        :label="model.nom"
-                    />
-                </el-select>
-            </el-form-item>
-            <el-form-item
-                :label="$t('Training')"
-                v-show="selectedModel != null"
-            >
-                <el-select
-                    v-model="selectedTraining"
-                    @change="canviTraining"
-                >
-                    <el-option
-                        v-for="(training, i) in trainings" :key="i"
-                        :value="training.id"
-                        :label="formatData(training.dataRegistre)"
-                    />
-                </el-select>
-            </el-form-item>
-            <el-button
-                @click="generarEtiqueta"
-                color="var(--gaissa_green)"
-                v-show="selectedTraining != null"
-            >
-                {{ $t('Generate label') }}
-            </el-button>
-        </el-form><br>
-        <EnergyLabel
-            :pdfBase64="labelBase64"
-            v-show="labelBase64"
-        />
-    </div>
+    <PreSaved :fase="$t('Training')"/>
 </template>
 
 <script>
-    import models from '@/services/models'
-    import trainings from '@/services/trainings'
-    import {formatData} from '@/utils'
-    import EnergyLabel from "@/components/EnergyLabel.vue";
+    import PreSaved from "@/components/PreSaved.vue";
     export default {
         name: "TrainingPreSaved",
-        components: {EnergyLabel},
-        data() {
-            return {
-                models: null,
-                selectedModel: null,
-                trainings: null,
-                selectedTraining: null,
-                labelBase64: null
-            };
-        },
-        methods: {
-            async refrescaModels() {
-                const response = await models.list()
-                this.models = response.data
-            },
-            async refrescaEntrenaments() {
-                const response = await trainings.listByModel(this.selectedModel)
-                this.trainings = response.data
-            },
-            async canviModel() {
-                await this.refrescaEntrenaments()
-                this.selectedTraining = null
-                this.labelBase64 = null
-            },
-            async canviTraining() {
-                this.labelBase64 = null
-            },
-            async generarEtiqueta() {
-                const response = await trainings.retrieve(this.selectedModel, this.selectedTraining)
-                this.labelBase64 = response.data['energy_label']
-            },
-            formatData,
-        },
-        async mounted() {
-            await this.refrescaModels();
-        },
-    };
+        components: {PreSaved}
+    }
 </script>
 
 <style>
