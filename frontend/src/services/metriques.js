2aa1a8af322ebb399b527c2214b61c792cdab1a6
Pau Duran <pauduranmanzano@gmail.com>
2023-10-28 12:08:45 +0200

    Pantalla admin mètriques i informacions

--- frontend/src/services/metriques.js
+++ frontend/src/services/metriques.js
@@ -1,6 +1,10 @@
 import axios from "@/services/backend"
 
 export default {
+    async listOrdered() {
+        const responseMetriques = await axios.get(`/api/metriques/?ordering=-pes.json`)
+        return responseMetriques
+    },
     async listOrderedFilteredByPhase(phase) {
         const responseMetriques = await axios.get(`/api/metriques/?fase=${phase}&ordering=-pes.json`)
         return responseMetriques
