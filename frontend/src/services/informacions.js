d0c68ee447a5bf4ede67a1e248d049e37df0aa48
Pau Duran <pauduranmanzano@gmail.com>
2023-11-05 15:09:08 +0100

    Pantalla admin informació per editar

--- frontend/src/services/informacions.js
+++ frontend/src/services/informacions.js
@@ -9,6 +9,16 @@ export default {
         const responseInformacions = await axios.get(`/api/informacions/?fase=${phase}`)
         return responseInformacions
     },
+    async getById(id) {
+        const responseInformacio = await axios.get(`/api/informacions/${id}.json`)
+        return responseInformacio
+    },
+    async update(data) {
+        const responseInformacio = await axios.put(`/api/informacions/${data.id}/`, data, {
+            responseType: 'json'
+        })
+        return responseInformacio
+    },
     async delete(id) {
         const responseInformacio = await axios.delete(`/api/informacions/${id}.json`)
         return responseInformacio
