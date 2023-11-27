b3d0910e6d1e0f1f0544b6cfed4a66fbead955c4
Pau Duran <pauduranmanzano@gmail.com>
2023-11-27 21:26:34 +0100

    Crida login front -> back i errors

---
+++ frontend/src/store.js
@@ -0,0 +1,23 @@
+/*import { createStore } from 'vuex'
+
+const store = createStore({
+    state: {
+        token: null,
+    },
+    getters: {
+        getToken(state) {
+            return state.token
+        },
+        isLogged(state) {
+            return state.token !== null
+        }
+    },
+    mutations: {
+        setToken(state, token) {
+            store.token = token
+        }
+    }
+})*/
+
+const store = 3
+export default store
