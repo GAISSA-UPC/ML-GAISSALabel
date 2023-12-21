63e31bde1c26f1a61c682267ad6e98e38c83b33d
Pau Duran <pauduranmanzano@gmail.com>
2023-12-21 13:23:36 +0100

    Nova estructura upload fitxers per triar eina de càlcul

--- frontend/src/components/FileNewExperiment.vue
+++ frontend/src/components/FileNewExperiment.vue
@@ -35,12 +35,36 @@
                     drag
                     style="width: 100%"
                     :auto-upload="false"
+                    :show-file-list="false"
                 >
                     <font-awesome-icon :icon="['fas', 'cloud-arrow-up']" />
                     <div class="el-upload__text">
                         {{ $t('Drop file here or') }} <em>{{ $t('click to upload') }}</em>
                     </div>
                 </el-upload>
+
+                <ul class="el-upload-list el-upload-list--text" style="width: 100%">
+                    <li v-for="(file, index) in fileList" :key="index" class="el-upload-list__item is-ready" tabindex="0">
+                        <div class="el-upload-list__item-info">
+                            <a class="el-upload-list__item-name">
+                                <i class="el-icon el-icon--document">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024">
+                                        <path fill="currentColor" d="M832 384H576V128H192v768h640zm-26.496-64L640 154.496V320zM160 64h480l256 256v608a32 32 0 0 1-32 32H160a32 32 0 0 1-32-32V96a32 32 0 0 1 32-32m160 448h384v64H320zm0-192h160v64H320zm0 384h384v64H320z"></path>
+                                    </svg>
+                                </i>
+                                <span class="el-upload-list__item-file-name" :title="file.name">{{ file.name }}</span>
+                                <el-select style="margin-left: 10px" v-model="file.tool" :placeholder="$t('Tool used')" :clearable="true">
+                                    <el-option key="hola" label="hola" value="hola" />
+                                </el-select>
+                            </a>
+                        </div>
+                        <el-button class="el-icon el-icon--close" @click="fileList.splice(index, 1)">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024">
+                                <path fill="currentColor" d="M764.288 214.592 512 466.88 259.712 214.592a31.936 31.936 0 0 0-45.12 45.12L466.752 512 214.528 764.224a31.936 31.936 0 1 0 45.12 45.184L512 557.184l252.288 252.288a31.936 31.936 0 0 0 45.12-45.12L557.12 512.064l252.288-252.352a31.936 31.936 0 1 0-45.12-45.184z"></path>
+                            </svg>
+                        </el-button>
+                    </li>
+                </ul>
             </el-form-item>
             <el-button
                 @click="gestioFitxers"
@@ -91,6 +115,9 @@ export default {
             if (this.fase === this.$t('Training')) response = await trainings.listByModel(this.selectedModel)
             else response = await inferencies.listByModel(this.selectedModel)
             this.experiments = response.data
+        },
+        async refrescaEines() {
+
         },
         async canviModel() {
             await this.refrescaExperiments()
