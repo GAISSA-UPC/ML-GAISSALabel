3a75d7293d65dffad6c5749cb9491f71337a113a
Pau Duran <pauduranmanzano@gmail.com>
2023-12-31 12:53:56 +0100

    Informació d'última sincro i proveïdors

--- frontend/src/services/sincronitzacions.js
+++ frontend/src/services/sincronitzacions.js
@@ -1,6 +1,10 @@
 import axios from "@/services/backend"
 
 export default {
+    async informacio() {
+        const responseInfo = await axios.get(`/api/sincronitzacio/`)
+        return responseInfo
+    },
     async sincronitzar() {
         const responseSincro = await axios.post(`/api/sincronitzacio/`).then(response => {return response})
     .catch(error => {return error})
