bbd3bec3f9162511ed13d449696129f6ae0c3b70
Pau Duran <pauduranmanzano@gmail.com>
2024-01-04 17:49:55 +0100

    Missatge d'error quan inference deploy no funciona

--- frontend/src/views/InferenceDeploy.vue
+++ frontend/src/views/InferenceDeploy.vue
@@ -2,6 +2,13 @@
     <h1>{{ $t("Energy label for inference") }}</h1><br>
     <h2>{{ $t("Register a new inference") }}</h2><br>
 
+    <div v-if="estat">
+        <el-alert v-if="estat === 'inference-ko'"
+                  :title="$t('There was an error while trying to inference the model you provided. Review both endpoint and input provided.')"
+                  type="error" @close="estat = ''"/>
+        <br>
+    </div>
+
     <el-form label-position="top">
         <h3 style="color: var(--gaissa_green);font-weight: bold">{{ $t("Endpoint where the model is deployed") }}</h3><br>
         <el-form-item>
@@ -31,15 +38,22 @@ export default {
         return {
             endpoint: null,
             input: null,
+            estat: '',
         };
     },
     methods: {
         async calcularEficiencia() {
             const resultats = await calculadors.calcularEficienciaInferencia(this.endpoint, this.input)
-            this.$router.push({
-                name: 'inference form',
-                query: {dadesInicials: JSON.stringify(resultats)}
-            })
+            console.log(resultats.status)
+            if (resultats.status === 201) {
+                this.$router.push({
+                    name: 'inference form',
+                    query: {dadesInicials: JSON.stringify(resultats.data)}
+                })
+            } else {
+                this.estat = 'inference-ko'
+                window.scrollTo({top:0})
+            }
         }
     }
 }
