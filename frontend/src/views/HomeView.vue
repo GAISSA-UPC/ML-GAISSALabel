b2208af9982f398f57f2544b91b9961837cf09f5
Pau Duran <pauduranmanzano@gmail.com>
2024-01-05 17:36:44 +0100

    Primera versió pàgina home

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,10 +1,40 @@
 <script setup>
-import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 </script>
 
 <template>
-    <main>
-        <font-awesome-icon :icon="['fas', 'dumbbell']" />
-        Hooola
-    </main>
+    <h1>{{ $t('Welcome to GAISSALabel!') }}</h1><br>
+    <h2 style="text-align: justify">{{  $t('The tool you are seeking for to easy assess the footprint of ML models and help you improve your models\' efficiency.') }}</h2><br>
+
+    <h3>{{ $t('GAISSALabel is a tool that can assist you in evaluating both the training and inference phases of any machine learning model. \
+    To navigate through the various available pages, utilize the left menu. \
+    During both the training and inference phases, you have the capability to generate efficiency results for your models or retrieve existing ones.') }}</h3><br>
+    <h3>{{ $t('For now, we have registered') }}</h3><br>
+    <el-row justify="center" :gutter="30">
+        <el-col :span="8">
+            <el-card class="box-card" style="background-color: var(--gaissa_green_light);text-align: center">
+                <h1 style="color: var(--gaissa_green)">3546</h1>
+                <h2 style="color: var(--gaissa_green)">{{ $t('models') }}</h2>
+            </el-card>
+        </el-col>
+        <el-col :span="8">
+            <el-card class="box-card" style="background-color: var(--gaissa_green_light);text-align: center">
+                <h1 style="color: var(--gaissa_green)">3546</h1>
+                <h2 style="color: var(--gaissa_green)">{{ $t('trainings') }}</h2>
+            </el-card>
+        </el-col>
+        <el-col :span="8">
+            <el-card class="box-card" style="background-color: var(--gaissa_green_light);text-align: center">
+                <h1 style="color: var(--gaissa_green)">3546</h1>
+                <h2 style="color: var(--gaissa_green)">{{ $t('inferences') }}</h2>
+            </el-card>
+        </el-col>
+    </el-row><br>
+    <h3>{{ $t('We appreciate your trust. \
+    Let us start the journey towards creating more environmentally friendly machine learning models!') }}</h3><br>
 </template>
+
+<style>
+h3 {
+    text-align: justify
+}
+</style>
