75ffe9ac3e3adb326d523acd979a29c2bbab215e
Pau Duran <pauduranmanzano@gmail.com>
2024-01-06 18:39:59 +0100

    Graphviz per diagrama de classes BD

--- backend/gaissalabel/settings.py
+++ backend/gaissalabel/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'django_extensions',
     'django_filters',
     'corsheaders',
     'drf_yasg',
