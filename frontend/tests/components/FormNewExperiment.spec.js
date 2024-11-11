a24ae54185b46072abb1f6c9de6d3e7eb60d7b19
pol.plana <123639572+pol-33@users.noreply.github.com>
2024-11-11 13:08:14 +0100

    Testing Mocks Added

--- frontend/tests/components/FormNewExperiment.spec.js
+++ frontend/tests/components/FormNewExperiment.spec.js
@@ -3,11 +3,26 @@ import FormNewExperiment from '@/components/FormNewExperiment.vue';
 import ElementPlus from 'element-plus';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { i18n } from '@/i18n';
+import models from '@/controllers/models';
+import metriques from '@/controllers/metriques';
+import informacions from '@/controllers/informacions';
+
+// Import mocks
+vi.mock('@/controllers/models');
+vi.mock('@/controllers/metriques');
+vi.mock('@/controllers/informacions');
+
+// Mocks to return a specific response structure
+import { mockModelsData, mockMetriquesData, mockInformacionsData } from '@tests/components/FormNewExperiment.mock'; 
 
 describe('FormNewExperiment.vue', () => {
     let wrapper;
 
     beforeEach(() => {
+        models.list.mockResolvedValue(mockModelsData);
+        metriques.listOrderedFilteredByPhase.mockResolvedValue(mockMetriquesData);
+        informacions.listFilteredByPhase.mockResolvedValue(mockInformacionsData);
+
         wrapper = mount(FormNewExperiment, {
             global: {
                 plugins: [ElementPlus, i18n],
