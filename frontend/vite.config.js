eb513f8fe9d1e1b292d435b4d075d647ac867bf6
pol.plana <123639572+pol-33@users.noreply.github.com>
2024-11-21 03:13:02 +0100

    Enable Gzip Compression and Chunking

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,45 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    // Gzip compression
+    viteCompression({
+      algorithm: 'gzip',
+      ext: '.gz',
+      deleteOriginFile: false // Keep original uncompressed files
+    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@tests': fileURLToPath(new URL('./tests', import.meta.url))
-    }
+      '@tests': fileURLToPath(new URL('./tests', import.meta.url)),
+    },
   },
   test: { // Configuración para Vitest
     globals: true,
     environment: 'jsdom',
-  }
-})
+  },
+  build: {
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['vue', 'vue-router', 'vuex', 'axios'],
+          'element-plus': ['element-plus'],
+          fontawesome: [
+            '@fortawesome/fontawesome-svg-core',
+            '@fortawesome/free-regular-svg-icons',
+            '@fortawesome/free-solid-svg-icons',
+            '@fortawesome/free-brands-svg-icons',
+          ],
+          vuetify: ['vuetify'],
+        },
+      },
+    },
+  },
+});
