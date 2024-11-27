f9eac6b4736f2bef46cf99a0dc1d57aef270e9d7
pol.plana <123639572+pol-33@users.noreply.github.com>
2024-11-27 01:43:03 +0100

    Webp and resized images

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -8,7 +8,7 @@
     Your actions can help drive the development of more sustainable practices in machine learning, contributing towards a greener future. \
     Let\'s embark on this journey towards Green AI together!') }}</h3><br>
     <div style="display: flex;justify-content: center;width: 100%;margin-top: 50px">
-        <img src="/gaissalogo.png" style="max-width: 25%" alt="GAISSA logo"/>
+        <img src="/gaissalogo.webp" style="max-width: 25%" alt="GAISSA logo"/>
     </div>
 
 </template>
