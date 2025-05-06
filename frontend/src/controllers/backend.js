98fee70969a1db2a4382fd923a14328478213aab
pol.plana <123639572+pol-33@users.noreply.github.com>
2025-05-06 23:04:07 +0200

    Update backend.js

--- frontend/src/controllers/backend.js
+++ frontend/src/controllers/backend.js
@@ -6,7 +6,8 @@ axios.interceptors.request.use(
         config.headers['Accept'] = 'application/json'
         config.headers['Content-Type'] = 'application/json'
         if (store.getters.isLogged) config.headers['Authorization'] = 'Token ' + store.getters.getToken
-        config.baseURL = 'http://localhost:8000'
+        //config.baseURL = 'http://localhost:8000'
+        config.baseURL = 'http://gaissalabel.essi.upc.edu:81'
         return config
     },
     error => {
