481645f27b48f69d27a499ea15ac04c650c58eef
pol.plana <123639572+pol-33@users.noreply.github.com>
2025-06-09 03:29:20 +0200

    Project folder name references changed

--- backend/gaissa_tools/urls.py
+++ backend/gaissa_tools/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for gaissalabel project.
+URL configuration for gaissa_tools project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
